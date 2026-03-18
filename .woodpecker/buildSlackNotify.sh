9141c5c64d05d0800b5bb7168a0bf0355887641d
yaroslav8765 <pechorkin2014@gmail.com>
2026-03-18 12:57:48 +0200

    fix: Implement code changes to enhance functionality and improve performance

--- .woodpecker/buildSlackNotify.sh
+++ .woodpecker/buildSlackNotify.sh
@@ -4,8 +4,10 @@ set -x
 
 COMMIT_SHORT_SHA=$(echo $CI_COMMIT_SHA | cut -c1-8)
 
+STATUS=${1}  
 
-if [ "$CI_STEP_STATUS" = "success" ]; then
+
+if [ "$STATUS" = "success" ]; then
   MESSAGE="Did a build without issues on \`$CI_REPO_NAME/$CI_COMMIT_BRANCH\`. Commit: _${CI_COMMIT_MESSAGE}_ (<$CI_COMMIT_URL|$COMMIT_SHORT_SHA>)"
 
   curl  -s -X POST -H "Content-Type: application/json" -d '{
